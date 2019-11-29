c448fcdd2b319e94f098dea010304f47aec64144
Opeyemi Adenola <yemi@Opeyemis-MacBook-Pro.local>
2019-11-29 13:03:48 +0000

    layout directory and added nav bar to App

--- realnews/src/App.js
+++ realnews/src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import {Router}  from '@reach/router';
 import './App.css';
 import Homepage from './Components/Homepage'
-
+import Layout from './Layouts/Layout'
+import NavigationBar from './Layouts/Navigation';
 
 function App() {
   return (
     <>
+      <NavigationBar/>
+      <Layout>
      <Router>
        <Homepage path="/"/>
      </Router>
+     </Layout>
     </>
   );
 }
