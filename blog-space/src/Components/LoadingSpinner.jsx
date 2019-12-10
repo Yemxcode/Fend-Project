eb12cd6b16d5ebf7303aae7fd65346dd0b539e65
Opeyemi Adenola <yemi@Opeyemis-MacBook-Pro.local>
2019-12-10 09:24:07 +0000

    created a basic spinner

--- blog-space/src/Components/LoadingSpinner.jsx
+++ blog-space/src/Components/LoadingSpinner.jsx
@@ -3,10 +3,15 @@ import '../Layouts/Spinner.css'
 
 export default function LoadingSpinner () {
  return (
-  <div className="loadingio-spinner-spin-pssc9fth0uk"><div className="ldio-iupe6kelu2">
-<div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
-</div></div>
+     <div className="loadingio-spinner-spin-pssc9fth0uk"><div className="ldio-iupe6kelu2">
+   <div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div><div><div></div></div>
+   </div></div>
 
-
- )
+   // <div className="spinner">
+   //   <div className="spinner-text">Loading</div>
+   //   <div className="spinner-sector spinner-sector-one"></div>
+   //   <div className="spinner-sector spinner-sector-two"></div>
+   //   <div className="spinner-sector spinner-sector-three"></div>
+   // </div>
+ );
 }
