de8872c8de27eca9f3461337e9bc04e3509faad6
Opeyemi Adenola <yemi@Opeyemis-MacBook-Pro.local>
2019-12-12 10:36:15 +0000

    created a login page that sets the state for the context

--- blog-space/src/Components/LogInPage.jsx
+++ blog-space/src/Components/LogInPage.jsx
@@ -1,41 +1,38 @@
-import React from 'react';
-import { Context } from '../MyContext';
+
+
+import React from "react";
+import { Context } from "../MyContext";
 
 export default class LogInPage extends React.Component {
- state = {
-  username: ''
- }
+  state = {
+    username: ""
+  };
 
+  render() {
+    return (
+      <>
+        <Context.Consumer>
+          {context => (
+            <form
+              onSubmit={e => {
+                e.preventDefault();
+                context.logIn(this.state.username);
+              }}
+            >
+              <input
+                type="text"
+                required
+                value={this.state.username}
+                onChange={event =>
+                  this.setState({ username: event.target.value })
+                }
+              />
 
- // handleSubmit = (event) => {
- //  event.preventDefault();
- //  console.dir(event.target)
- // }
-
-//  {(context) => (<><p>i'm in side the consumer {context.state.loggedInAs}</p>
-//   <input type="text" onChange={context.logIn} /></>)}
-
-// </Context.Consumer>
-// (context) => (context.logIn(this.state.username))
-// (context) => (context.logIn(this.state.username))
-//  < Context.Consumer >
- render () {
-  
-  return (
- <>
-  <Context.Consumer>
-     {(context) => (
-      <form onSubmit={() => context.logIn(this.state.username)} >
-    <input type="text" required value={this.state.username} onChange={event => this.setState({username: event.target.value})}/>
-    
-    <button type="submit">Log In</button>
-   </form>
-     )}
-    
-   </Context.Consumer>
-   </>
-  )
- }
-
-
+              <button type="submit">Log In</button>
+            </form>
+          )}
+        </Context.Consumer>
+      </>
+    );
+  }
 }
--- blog-space/src/MyContext.jsx
+++ blog-space/src/MyContext.jsx
@@ -5,23 +5,42 @@ export const Context = React.createContext();
 
 export default class MyContext extends React.Component {
  state = {
-  loggedInAs: 'Guest'
+  loggedInAs: ''
  }
-
  
  
  render() {
   console.log(this.state.loggedInAs)
   return(
+   <>
    <Context.Provider value={
     {
      state: this.state,
-     logIn:(value) => this.setState({loggedInAs : value})
+          logIn: (value) => { this.setState({ loggedInAs: value})}
     }
    }>
    {this.props.children}
   </Context.Provider>
+   
+   </>
   )
   
  }
 }
+
+
+// import React, { useState, createContext } from 'react';
+
+
+// export const Context = React.createContext();
+
+// export const MyContext = props => {
+//   const [user, setUser] = useState([{ username: '' }]);
+
+
+//   return (
+//     <Context.Provider value={[user, setUser]}>
+//       {props.children}
+//     </Context.Provider>
+//   )
+// }
