8e75e1b4a83b04b5fc744ef81c40228fdbafe31c
Opeyemi Adenola <yemi@Opeyemis-MacBook-Pro.local>
2020-01-09 14:30:19 +0000

    basic footer with icons

--- blog-space/src/App.js
+++ blog-space/src/App.js
@@ -37,7 +37,7 @@ function App() {
             />
           </Router>
         </Layout>
-        {/* <Footer/> */}
+        <Footer/>
       </MyContext>
     </>
   );
--- blog-space/src/Components/Footer.jsx
+++ blog-space/src/Components/Footer.jsx
@@ -1,21 +1,59 @@
-
-import React from 'react';
-import '../Layouts/Main.css'
-
+import React from "react";
+import "../Layouts/Main.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {  faFutbol } from "@fortawesome/free-solid-svg-icons";
 
 export default function Footer() {
+  return (
+    <footer className="page-footer font-small cyan darken-3">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12 py-5">
+            <div className="mb-5 flex-center">
+              
+                
+                  <FontAwesomeIcon icon={faFutbol} />
+                  {" "}
+                
+              
 
- return (
+              
+                
+                  <FontAwesomeIcon icon={faFutbol} />
+                  {" "}
+               
 
+              
+                
+                  <FontAwesomeIcon icon={faFutbol} />
+                  
+              
 
-  <footer className="page-footer font-small special-color-dark pt-4">
+              
+                
+                  <FontAwesomeIcon icon={faFutbol} />
+                  {" "}
+                
+              
 
+              
+                
+                  <FontAwesomeIcon icon={faFutbol} />
+                  {" "}
+                
+              
+            </div>
+          </div>
+        </div>
+      </div>
 
-  
-</footer>
- 
- 
- )
-
+      <div className="footer-copyright text-center py-3">
+        © 2020 Copyright:
+        
+          {" "}
+          blogspace.com
+        
+      </div>
+    </footer>
+  );
 }
-
