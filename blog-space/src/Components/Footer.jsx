d41fe2255510f55b905ad93e878c238ed288ba79
Opeyemi Adenola <yemi@opeyemis-mbp.home>
2020-01-13 22:15:04 +0000

    added a footer with css

--- blog-space/src/Components/Footer.jsx
+++ blog-space/src/Components/Footer.jsx
@@ -1,59 +1,40 @@
 import React from "react";
 import "../Layouts/Main.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {  faFutbol } from "@fortawesome/free-solid-svg-icons";
+import { faFacebook, faTwitter, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
     <footer className="page-footer font-small cyan darken-3">
-      <div className="container">
+      <div className="footerContainer">
         <div className="row">
           <div className="col-md-12 py-5">
             <div className="mb-5 flex-center">
-              
-                
-                  <FontAwesomeIcon icon={faFutbol} />
-                  {" "}
-                
-              
-
-              
-                
-                  <FontAwesomeIcon icon={faFutbol} />
-                  {" "}
-               
-
-              
-                
-                  <FontAwesomeIcon icon={faFutbol} />
-                  
-              
-
-              
-                
-                  <FontAwesomeIcon icon={faFutbol} />
-                  {" "}
-                
-              
-
-              
-                
-                  <FontAwesomeIcon icon={faFutbol} />
-                  {" "}
-                
+              <FontAwesomeIcon
+                icon={faFacebook}
+                className="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"
+              />{" "}
+              <FontAwesomeIcon
+                icon={faTwitter}
+                className="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"
+              />{" "}
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"
+              />
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                className="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"
+              />{" "}
               
             </div>
           </div>
         </div>
       </div>
 
-      <div className="footer-copyright text-center py-3">
-        © 2020 Copyright:
-        
-          {" "}
-          blogspace.com
-        
-      </div>
+      <p className="footer-copyright text-center py-3">
+        © 2020 Copyright: blogspace.com
+      </p>
     </footer>
   );
 }
